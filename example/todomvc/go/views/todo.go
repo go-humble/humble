8e4ab2f839b38d1534e6d4c0950e70d551524a44
Soroush Pour <me@soroushjp.com>
2015-01-26 03:58:18 +1100

    Implemented todo UI and model update on double click and edit by user

--- example/todomvc/go/views/todo.go
+++ example/todomvc/go/views/todo.go
@@ -13,17 +13,26 @@ type Todo struct {
 }
 
 func (t *Todo) RenderHTML() string {
-	return fmt.Sprintf(`<li>
+	return fmt.Sprintf(`<li class="todo-list-item">
 		<input class="toggle" type="checkbox" %s>
-		<label>%s</label>
+		<label class="todo-label">%s</label>
 		<button class="destroy"></button>
-		<input class="edit" value="%s">
+		<input class="edit" onfocus="this.value = this.value;" value="%s">
 		</li>`,
 		t.Model.CheckedStr(), t.Model.Title, t.Model.Title)
 }
 
 func (t *Todo) OnLoad() error {
-	err := humble.Views.AddListener(t, "button.destroy", "click", t.deleteButtonClicked)
+	var err error
+	err = humble.Views.AddListener(t, "button.destroy", "click", t.deleteButtonClicked)
+	if err != nil {
+		panic(err)
+	}
+	err = humble.Views.AddListener(t, "li.todo-list-item", "dblclick", t.todoDoubleClick)
+	if err != nil {
+		panic(err)
+	}
+	err = humble.Views.AddListener(t, "input.edit", "keyup", t.todoEditKeyUp)
 	if err != nil {
 		panic(err)
 	}
@@ -32,6 +41,58 @@ func (t *Todo) OnLoad() error {
 
 func (t *Todo) OuterTag() string {
 	return "div"
+}
+
+func (t *Todo) todoEditKeyUp(event dom.Event) {
+	// If key is not Enter or Escape, we keep label and input.edit in sync but otherwise just return
+	key := event.(*dom.KeyboardEvent).KeyCode
+	// Grab contents of input.edit
+	inputEdit, err := humble.Views.ChildQuerySelector(t, "input.edit")
+	if err != nil {
+		panic(err)
+	}
+	title := inputEdit.Underlying().Get("value").String()
+	if key != EnterKey && key != EscapeKey {
+		// Change everything in label to match input.edit
+		label, err := humble.Views.ChildQuerySelector(t, "label.todo-label")
+		if err != nil {
+			panic(err)
+		}
+		label.SetInnerHTML(title)
+		return
+	}
+	// If Escape or Enter key is entered, we want to get out of input.edit field
+	if key == EscapeKey || key == EnterKey {
+		todoItem, err := humble.Views.ChildQuerySelector(t, "li.todo-list-item")
+		if err != nil {
+			panic(err)
+		}
+		//Remove 'editing' class to todoItem to make it disappear
+		todoItem.Class().Remove("editing")
+	}
+	// If Enter key is pressed, we want to save to model
+	if key == EnterKey {
+		t.Model.Title = title
+		if err := humble.Models.Update(t.Model); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (t *Todo) todoDoubleClick(dom.Event) {
+	// Get elements
+	todoItem, err := humble.Views.ChildQuerySelector(t, "li.todo-list-item")
+	if err != nil {
+		panic(err)
+	}
+	inputEdit, err := humble.Views.ChildQuerySelector(t, "input.edit")
+	if err != nil {
+		panic(err)
+	}
+	// Append 'editing' class to todoItem to make it an editable input field
+	todoItem.Class().Add("editing")
+	// Set focus to input.edit field
+	inputEdit.(dom.HTMLElement).Focus()
 }
 
 func (t *Todo) deleteButtonClicked(dom.Event) {
