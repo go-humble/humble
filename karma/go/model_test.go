dae9e97386dd15a90c012c503e9168641113e35a
Alex Browne <stephenalexbrowne@gmail.com>
2015-04-18 19:21:30 -0400

    Write test for model.ReadAll

--- karma/go/model_test.go
+++ karma/go/model_test.go
@@ -2,14 +2,51 @@ package main
 
 import (
 	"github.com/JohannWeging/jasmine"
+	"github.com/soroushjp/humble/model"
+	"strconv"
 )
 
+type Todo struct {
+	Id          int
+	Title       string
+	IsCompleted bool
+}
+
+func (t Todo) GetId() string {
+	return strconv.Itoa(t.Id)
+}
+
+func (t Todo) RootURL() string {
+	return "http://localhost:3000/todos"
+}
+
 func main() {
-	// This test is just checks that the code cross-compiled correctly and can
-	// be executed by the karma test runner.
-	jasmine.Describe("Tests", func() {
-		jasmine.It("can be loaded", func() {
-			jasmine.Expect(true).ToBe(true)
+	jasmine.Describe("ReadAll", func() {
+		jasmine.It("gets all the existing todos", func() {
+			expectedModels := []*Todo{
+				{
+					Id:          0,
+					Title:       "Write a frontend framework in Go",
+					IsCompleted: false,
+				},
+				{
+					Id:          1,
+					Title:       "???",
+					IsCompleted: false,
+				},
+				{
+					Id:          2,
+					Title:       "Profit!",
+					IsCompleted: false,
+				},
+			}
+			go func() {
+				gotModels := []*Todo{}
+				err := model.ReadAll(&gotModels)
+				jasmine.Expect(err).ToEqual(nil)
+				jasmine.Expect(gotModels).ToEqual(expectedModels)
+			}()
 		})
 	})
+
 }
