ba222134cdcd7ac7d0d422e3f2f5ae110b1e32d2
Alex Browne <stephenalexbrowne@gmail.com>
2015-04-22 23:40:19 -0400

    Refactor model.go

--- model/model.go
+++ model/model.go
@@ -22,32 +22,27 @@ type Model interface {
 // of some concrete type. It will use the RootURL() method of the models to
 // figure out which url to send the GET request to.
 func ReadAll(models interface{}) error {
+	rootURL := getURLFromModels(models)
+	return sendRequestWithoutDataAndUnmarshal("GET", rootURL, models)
+}
+
+// getURLFromModels returns the url that should be used for the type that corresponds
+// to models. It does this by instantiating a new model of the correct type and then
+// calling RootURL on it. models should be a pointer to a slice of models.
+func getURLFromModels(models interface{}) string {
 	// We expect some pointer to a slice of models. Like *[]Todo
-	// First Elem() givew us []Todo
-	// Second Elem() gives us Todo
+	// First Elem() givew us a slice of Model
+	// Second Elem() gives us Model
 	modelsType := reflect.TypeOf(models).Elem()
 	// TODO: type checking!
 	modelTypePtr := modelsType.Elem()
 	modelType := modelTypePtr.Elem()
-	// reflect.New returns *Todo
+	// reflect.New returns a pointer to a Model
 	newModelVal := reflect.New(modelType)
 	newModelInterface := newModelVal.Interface()
 	newModel := newModelInterface.(Model)
 	// TODO: check for a failed type assertion!
-	urlRoot := newModel.RootURL()
-	// Create request
-	res, err := http.Get(urlRoot)
-	if err != nil {
-		return fmt.Errorf("Something went wrong with GET request to %s: %s", urlRoot, err.Error())
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("Couldn't read response to %s: %s", urlRoot, err.Error())
-	}
-	if err := json.Unmarshal(body, models); err != nil {
-		return fmt.Errorf("Failed to unmarshal response into object, with Error: %s.\nResponse was: %s", err, string(body))
-	}
-	return nil
+	return newModel.RootURL()
 }
 
 // Read will send a GET request to a RESTful server to get the model by the given id,
@@ -57,14 +52,7 @@ func ReadAll(models interface{}) error {
 // like "http://hostname.com/todos/123"
 func Read(id string, model Model) error {
 	fullURL := model.RootURL() + "/" + id
-	res, err := http.Get(fullURL)
-	if err != nil {
-		return fmt.Errorf("Something went wrong with GET request to %s: %s", fullURL, err.Error())
-	}
-	if err := unmarshalResponse(res, model); err != nil {
-		return err
-	}
-	return nil
+	return sendRequestWithoutDataAndUnmarshal("GET", fullURL, model)
 }
 
 // Create expects a pointer some concrete type which implements Model (e.g., *Todo).
@@ -75,14 +63,7 @@ func Read(id string, model Model) error {
 func Create(model Model) error {
 	fullURL := model.RootURL()
 	encodedString := encodeModelFields(model)
-	res, err := http.Post(fullURL, "application/x-www-form-urlencoded", strings.NewReader(encodedString))
-	if err != nil {
-		return fmt.Errorf("Something went wrong with POST request to %s. %s", fullURL, err.Error())
-	}
-	if err := unmarshalResponse(res, model); err != nil {
-		return err
-	}
-	return nil
+	return sendRequestWithDataAndUnmarshal("POST", fullURL, encodedString, model)
 }
 
 // Update expects a pointer some concrete type which implements Model (e.g., *Todo), with a model.Id
@@ -93,19 +74,7 @@ func Create(model Model) error {
 func Update(model Model) error {
 	fullURL := model.RootURL() + "/" + model.GetId()
 	encodedString := encodeModelFields(model)
-	req, err := http.NewRequest("PUT", fullURL, strings.NewReader(encodedString))
-	if err != nil {
-		return fmt.Errorf("Something went wrong building PUT request to %s. %s", fullURL, err.Error())
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("Something went wrong with PUT request to %s. %s", fullURL, err.Error())
-	}
-	if err := unmarshalResponse(res, model); err != nil {
-		return err
-	}
-	return nil
+	return sendRequestWithDataAndUnmarshal("PUT", fullURL, encodedString, model)
 }
 
 // Delete expects a pointer some concrete type which implements Model (e.g., *Todo).
@@ -118,25 +87,64 @@ func Delete(model Model) error {
 	fullURL := model.RootURL() + "/" + model.GetId()
 	req, err := http.NewRequest("DELETE", fullURL, nil)
 	if err != nil {
-		return fmt.Errorf("Something went wrong building DELETE request to %s. %s", fullURL, err.Error())
+		return fmt.Errorf("Something went wrong building DELETE request to %s: %s", fullURL, err.Error())
 	}
 	if _, err := http.DefaultClient.Do(req); err != nil {
-		return fmt.Errorf("Something went wrong with DELETE request to %s. %s", fullURL, err.Error())
+		return fmt.Errorf("Something went wrong with DELETE request to %s: %s", fullURL, err.Error())
 	}
 	return nil
 }
 
+// sendRequestWithoutDataAndUnmarshal creates a request with the given method and url, sends
+// it using the default client, and then marshals the json response into v.
+func sendRequestWithoutDataAndUnmarshal(method string, url string, v interface{}) error {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return fmt.Errorf("Something went wrong building %s request to %s: %s", method, url, err.Error())
+	}
+	return sendRequestAndUnmarshal(req, v)
+}
+
+// sendRequestWithDataAndUnmarshal creates a request with the given method, url, and data, sends
+// it using the default client, and then marshals the json response into v. It uses the Content-Type
+// header application/x-www-form-urlencoded.
+func sendRequestWithDataAndUnmarshal(method string, url string, data string, v interface{}) error {
+	req, err := buildRequestWithData(method, url, data)
+	if err != nil {
+		return err
+	}
+	return sendRequestAndUnmarshal(req, v)
+}
+
+// buildRequestWithData creates and returns a request with the given method, url, and data. It
+// also sets the Content-Type header to application/x-www-form-urlencoded.
+func buildRequestWithData(method, url, data string) (*http.Request, error) {
+	req, err := http.NewRequest(method, url, strings.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("Something went wrong building %s request to %s: %s", method, url, err.Error())
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	return req, nil
+}
+
+// sendRequestAndUnmarshal sends req using http.DefaultClient and then marshals the response into v.
+// TODO: do something if the response status code is non-200.
+func sendRequestAndUnmarshal(req *http.Request, v interface{}) error {
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("Something went wrong with %s request to %s: %s", req.Method, req.URL.String(), err.Error())
+	}
+	return unmarshalResponse(res, v)
+}
+
 // unmarshalResponse reads the data from the body of res and then uses the json package to
-// unmarshal the data into model.
-func unmarshalResponse(res *http.Response, model Model) error {
+// unmarshal the data into v.
+func unmarshalResponse(res *http.Response, v interface{}) error {
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return fmt.Errorf("Couldn't read response to %s: %s", res.Request.URL.String(), err.Error())
 	}
-	if err := json.Unmarshal(body, model); err != nil {
-		return fmt.Errorf("Failed to unmarshal response into model, with error: %s.\nResponse was: %s", err, string(body))
-	}
-	return nil
+	return json.Unmarshal(body, v)
 }
 
 // encodeModelFields returns the fields of model represented as a url-encoded string.
