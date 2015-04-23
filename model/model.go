39715630cae1db9e86c7ab182fff58a0709c8042
Alex Browne <stephenalexbrowne@gmail.com>
2015-04-22 22:52:02 -0400

    Make model code DRYer. Re-implement Update using net/http

--- model/model.go
+++ model/model.go
@@ -62,12 +62,46 @@ func Read(id string, model Model) error {
 	if err != nil {
 		return fmt.Errorf("Something went wrong with GET request to %s: %s", fullURL, err.Error())
 	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("Couldn't read response to %s: %s", fullURL, err.Error())
+	if err := unmarshalResponse(res, model); err != nil {
+		return err
 	}
-	if err := json.Unmarshal(body, model); err != nil {
-		return fmt.Errorf("Failed to unmarshal response into object, with Error: %s.\nResponse was: %s", err, string(body))
+	return nil
+}
+
+// Create expects a pointer some concrete type which implements Model (e.g., *Todo).
+// It will send a POST request to the RESTful server. It expects a JSON containing the
+// created object from the server if the request was successful, and will set the fields of
+// model with the data in the response object. It will use the RootURL() method of
+// the model to determine which url to send the POST request to.
+func Create(model Model) error {
+	fullURL := model.RootURL()
+	encodedString := encodeModelFields(model)
+	res, err := http.Post(fullURL, "application/x-www-form-urlencoded", strings.NewReader(encodedString))
+	if err != nil {
+		return fmt.Errorf("Something went wrong with POST request to %s. %s", fullURL, err.Error())
+	}
+	if err := unmarshalResponse(res, model); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Update expects a pointer some concrete type which implements Model (e.g., *Todo), with a model.Id
+// that matches a stored object on the server. It will send a PUT request to the RESTful server.
+// It expects a JSON containing the updated object from the server if the request was successful,
+// and will set the fields of model with the data in the response object.
+// It will use the RootURL() method of the model to determine which url to send the PUT request to.
+func Update(model Model) error {
+	fullURL := model.RootURL() + "/" + model.GetId()
+	encodedString := encodeModelFields(model)
+	req, err := http.NewRequest("PUT", fullURL, strings.NewReader(encodedString))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("Something went wrong with PUT request to %s. %s", fullURL, err.Error())
+	}
+	if err := unmarshalResponse(res, model); err != nil {
+		return err
 	}
 	return nil
 }
@@ -90,55 +124,24 @@ func Delete(model Model) error {
 	return nil
 }
 
-// Create expects a pointer some concrete type which implements Model (e.g., *Todo).
-// It will send a POST request to the RESTful server. It expects a JSON containing the
-// created object from the server if the request was successful, and will set the fields of
-// model with the data in the response object. It will use the RootURL() method of
-// the model to determine which url to send the POST request to.
-func Create(model Model) error {
-	fullURL := model.RootURL()
-	bodyString := ""
-	// TODO: Do stronger type checking to prevent errors
-	//Use reflect to identify fields of our model and convert to URL-encoded formdata string
-	modelValPtr := reflect.ValueOf(model)
-	modelVal := modelValPtr.Elem()
-	modelValType := modelVal.Type()
-	for i := 0; i < modelValType.NumField(); i++ {
-		field := modelVal.Field(i)
-		fieldName := modelValType.Field(i).Name
-		value := fmt.Sprint(field.Interface())
-		bodyString += fieldName + "=" + url.QueryEscape(value)
-		if i != modelValType.NumField()-1 {
-			bodyString += "&"
-		}
-	}
-	res, err := http.Post(fullURL, "application/x-www-form-urlencoded", strings.NewReader(bodyString))
-	if err != nil {
-		return fmt.Errorf("Something went wrong with POST request to %s. %s", fullURL, err.Error())
-	}
-	// Unmarshal our server response object into our model
+// unmarshalResponse reads the data from the body of res and then uses the json package to
+// unmarshal the data into model.
+func unmarshalResponse(res *http.Response, model Model) error {
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return fmt.Errorf("Couldn't read response to %s: %s", fullURL, err.Error())
+		return fmt.Errorf("Couldn't read response to %s: %s", res.Request.URL.String(), err.Error())
 	}
-	err = json.Unmarshal(body, model)
-	if err != nil {
-		return fmt.Errorf("Failed to unmarshal response into object, with Error: %s.\nResponse was: %s", err, string(body))
+	if err := json.Unmarshal(body, model); err != nil {
+		return fmt.Errorf("Failed to unmarshal response into model, with error: %s.\nResponse was: %s", err, string(body))
 	}
 	return nil
 }
 
-// Update expects a pointer some concrete type which implements Model (e.g., *Todo), with a model.Id
-// that matches a stored object on the server. It will send a PUT request to the RESTful server.
-// It expects a JSON containing the updated object from the server if the request was successful,
-// and will set the fields of model with the data in the response object.
-// It will use the RootURL() method of the model to determine which url to send the PUT request to.
-func Update(model Model) error {
-	//Set our request URL to be root URL/Id, eg. example.com/api/todos/4
-	fullURL := model.RootURL() + "/" + model.GetId()
-	bodyString := ""
+// encodeModelFields returns the fields of model represented as a url-encoded string.
+// Suitable for POST requests with a content type of application/x-www-form-urlencoded
+func encodeModelFields(model Model) string {
 	// TODO: Do stronger type checking to prevent errors
-	//Use reflect to identify fields of our model and convert to URL-encoded formdata string
+	result := ""
 	modelValPtr := reflect.ValueOf(model)
 	modelVal := modelValPtr.Elem()
 	modelValType := modelVal.Type()
@@ -146,25 +149,10 @@ func Update(model Model) error {
 		field := modelVal.Field(i)
 		fieldName := modelValType.Field(i).Name
 		value := fmt.Sprint(field.Interface())
-		bodyString += fieldName + "=" + url.QueryEscape(value)
+		result += fieldName + "=" + url.QueryEscape(value)
 		if i != modelValType.NumField()-1 {
-			bodyString += "&"
+			result += "&"
 		}
 	}
-	// Create our x-www-form-urlencoded PUT request
-	req := xhr.NewRequest("PUT", fullURL)
-	req.Timeout = 1000 // one second, in milliseconds
-	req.ResponseType = "text"
-	req.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-	// Send our request
-	err := req.Send(bodyString)
-	if err != nil {
-		return fmt.Errorf("Something went wrong with PUT request to %s. %s", fullURL, err.Error())
-	}
-	// Unmarshal our server response object into our model
-	err = json.Unmarshal([]byte(req.Response.String()), model)
-	if err != nil {
-		return fmt.Errorf("Failed to unmarshal response into object, with Error: %s.\nResponse was: %s", err, req.Response.String())
-	}
-	return nil
+	return result
 }
