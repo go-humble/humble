c75996a39a2b601ec9322fd1453e246732fbf1d6
Alex Browne <stephenalexbrowne@gmail.com>
2015-05-12 00:08:29 -0400

    Fix bug in InterceptLinks and make it more effecient.

--- router/router.go
+++ router/router.go
@@ -131,26 +131,27 @@ func (r *Router) Back() {
 // instead of requesting a new page from the server.
 func (r *Router) InterceptLinks() {
 	for _, link := range document.Links() {
-		link.AddEventListener("click", true, func(event dom.Event) {
-			href := link.GetAttribute("href")
-			switch {
-			case href == "":
-				return
-			case strings.HasPrefix(href, "http://"), strings.HasPrefix(href, "https://"), strings.HasPrefix(href, "//"):
-				// These are external links and should behave normally.
-				return
-			case strings.HasPrefix(href, "#"):
-				// These are anchor links and should behave normally.
-				// Recall that even when we are using the hash trick, href
-				// attributes should be relative paths without the "#" and
-				// router will handle them appropriately.
-				return
-			case strings.HasPrefix(href, "/"):
-				// These are relative links. The kind that we want to intercept.
+		href := link.GetAttribute("href")
+		switch {
+		case href == "":
+			return
+		case strings.HasPrefix(href, "http://"), strings.HasPrefix(href, "https://"), strings.HasPrefix(href, "//"):
+			// These are external links and should behave normally.
+			return
+		case strings.HasPrefix(href, "#"):
+			// These are anchor links and should behave normally.
+			// Recall that even when we are using the hash trick, href
+			// attributes should be relative paths without the "#" and
+			// router will handle them appropriately.
+			return
+		case strings.HasPrefix(href, "/"):
+			// These are relative links. The kind that we want to intercept.
+			link.AddEventListener("click", true, func(event dom.Event) {
 				event.PreventDefault()
 				go r.Navigate(href)
-			}
-		})
+			})
+		}
+
 	}
 }
 
